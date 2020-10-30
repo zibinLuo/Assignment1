7483bb2e57de08017ad90c75b885ae6724164b6e
zibinLuo <1824912827@qq.com>
2020-10-30 14:56:49 +0800

    Added an index page

---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assignment1</title>
+	</head>
+	<body>
+		<h1>Fc Barcelona</h1>
+		<p><a href="/travel">Members of Barcelona football team</a>.</p>
+		
+	</body>
+</html>
