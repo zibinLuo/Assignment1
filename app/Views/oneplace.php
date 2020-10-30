4e044348d911ffe0d26d3c21f5aec8475274214e
zibinLuo <1824912827@qq.com>
2020-10-30 15:00:09 +0800

    Added an index page

---
+++ app/Views/oneplace.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assignment1</title>
+	</head>
+	<body>
+		<h1>The players information</h1>
+		<p><strong>Id:</strong> {id}</p>
+		<p><strong>Name:</strong> {name}</p>
+		<p><strong>Nationality:</strong> {description}</p>
+                <p><strong>Place of birth:</strong> {place of birth}</p>
+                <p><strong>Height and weight:</strong> {height and weight}</p>
+                 <p><strong>School of graduation:</strong> {school of graduation}</p>
+                 <p><strong>Professional characteristics:</strong> {Professional characteristics}</p>
+                <p><strong>Field position:</strong> {link}</p>
+                 <p><strong>Major awards:</strong> {major awards}</p>
+		<p><img src="/image/{image}"/></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
