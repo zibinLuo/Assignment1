c37bf1a64785be75c1b3895eb828369975f9c088
zibinLuo <1824912827@qq.com>
2020-10-30 15:00:04 +0800

    Added an index page

---
+++ app/Views/placeslist.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assignment1</title>
+	</head>
+	<body>
+		<h1>The player list</h1>
+		{records}
+		<p><a href="/travel/showme/{id}">{name}</a></p>
+		{/records}
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
